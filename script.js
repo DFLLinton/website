e51e19c92b6f72c050e1430c64859dc0dceddc2d
DFL Linton <106924177+DFLLinton@users.noreply.github.com>
2022-08-03 21:34:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
@@ -9,11 +12,15 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
- 
+  let slidetracker = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-
+  for (i = 0; i < slidetracker.length; i++) {
+    slidetracker[i].className = slidetracker[i].className.replace(" active", "");
+  }
   slides[slideIndex-1].style.display = "block";
-
+  slidetracker[slideIndex-1].className += " active";
 }
